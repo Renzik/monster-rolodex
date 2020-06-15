07e2e2f6f230f0df50b9786c6676278a8a051510
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2020-06-15 17:13:38 -0400

    Updates

---
+++ precache-manifest.04c4616b741694eeb099d8ad954c7479.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f86d770adf381040159113312140df",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1c73eb01dd9b58857f23",
+    "url": "/monster-rolodex/static/css/main.29dbe013.chunk.css"
+  },
+  {
+    "revision": "7488436fcb4d31289850",
+    "url": "/monster-rolodex/static/js/2.bab4244b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.bab4244b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c73eb01dd9b58857f23",
+    "url": "/monster-rolodex/static/js/main.758ffdf2.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  },
+  {
+    "revision": "b1448f7221d8906f1b2a43539760bbdd",
+    "url": "/monster-rolodex/static/media/Bisurk-Regular.b1448f72.ttf"
+  }
+]);
